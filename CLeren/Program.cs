7dfe7c6da28a86c931ba2c575bfa2c48550affe9
MasriNaser <Naser.Masri@cronos.be>
2022-09-19 19:15:39 +0200

    data type

---
+++ CLeren/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CLeren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+/*
+using System;
+
+namespace Number;
+
+
+internal class MyClass
+{
+    static void Main(string[] args)
+    {
+        string name = "Sanna";
+        Console.WriteLine(name);
+    }
+}
+
+*/
